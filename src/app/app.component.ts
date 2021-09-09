feb175c56e93891022333ffcdfd22c0151880958
munich99 <regerstrasse@icloud.com>
2021-09-09 10:11:34 +0200

    angular structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Daily } from './daily'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cuisine';
+
+  dailies : Daily[] =[{
+    menue: 'Ma-Po Tofu mit Hühnerhackfleisch',
+    preis: 8.5},
+    {
+    menue: 'Bärlauchtasche',
+    preis: 3,
+  }];
+
+  deleteDaily(daily:Daily) : void{
+    this.dailies = this.dailies.filter( a => a != daily)
+  }
+
+  putDaily(daily:Daily) : void{
+    this.dailies = this.dailies.filter( a => a != daily)
+  }
 }
